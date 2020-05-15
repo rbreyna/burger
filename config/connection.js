e07979f99ed9e3930de105a6b71f979c1d00f930
Roy Reyna <royreyna7@yahoo.com>
2020-05-14 21:54:43 -0500

    jaws db

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(proc.env.JAWSDB_URL);
+} else { mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "Titans2019!",
   database: "burgers_db"
 });
+}
 
 // Make connection.
 connection.connect(function(err) {
