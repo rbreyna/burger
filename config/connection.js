93da0c8eb94a3d25cfb586e453df8929677e2b6f
Roy Reyna <royreyna7@yahoo.com>
2020-05-16 13:16:12 -0500

    typo on connection

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var mysql = require("mysql");
 var connection;
 
 if(process.env.JAWSDB_URL){
-  connection = mysql.createConnection(proc.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else { mysql.createConnection({
   host: "localhost",
   port: 3306,
